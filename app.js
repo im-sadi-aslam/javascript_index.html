ca0633b8ee19df32fc93317b0f31505dfb364f34
im-sadi-aslam <sadafaslam249@gmail.com>
2024-12-17 23:39:32 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+
+alert("Welcome to my website!");
+
+
+ document.write("Welcome to my website!<br>Enjoy your visit.<br>Have a great day!");
+
+
+ alert("Welcome to my website!");
+ alert("Enjoy your visit.");
+ alert("Have a great day!");
+
+
+ console.log("Welcome to my website!");
+console.log("Enjoy your visit.");
+console.log("Have a great day!");
+
+
+alert("Welcome to my project!");
+
+
+alert("Script in <head> section is running!");
+
+
+alert("Script in <body> section is running!");
+
+alert("This alert is inside the page's HTML content!");
+
+alert("This alert is placed after the page’s HTML!");
+
+
+var username;
+
+var myName = "sadaf aslam";
+
+var message = "Hello World";
+alert(message);
+
+var studentName = "Sadaf Aslam";
+var studentAge = "20 years old";
+var studentCourse = "Web Development";
+
+alert("Name: " + studentName);
+alert("Age: " + studentAge);
+alert("Course: " + studentCourse);
+
+var alertMessage = "Congratulations!\nYou have successfully completed the task.";
+alert(alertMessage);
+
+var email = "example@example.com";
+alert("My email address is " + email);
+
+var book = "A smarter way to learn JavaScript";
+alert("I am learning from the book " + book);
+
+document.write("Yay! I can write HTML content through JavaScript.");
+
+var content = "This is an amazing JavaScript exercise!";
+alert(content);
+document.write(content);
+
